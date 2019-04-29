bf0a035d0beb3cb242a876f5a5ba4edbd5bff735
Anthony Rurup <tcrurup@gmail.com>
2019-04-29 21:51:16 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  
+  def name 
+    @this_dogs_name
+  end
+  
+  def name=(given_name)
+    @this_dogs_name = given_name
+  end
+  
+  def breed 
+    @this_dogs_breed
+  end
+  
+  def breed=(given_breed)
+    @this_dogs_breed = given_breed
+  end
+end
